a7df448cacc2930489cf5fa48c59ffd091c0d6ab
yanxianhe <xianhe_yan@sina.com>
2023-11-13 15:52:16 +0800

    [docs](add swagger map)

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 set -ex
+# mv ~/.local/lib/python3.10/site-packages/fastapi/openapi/docs.py ~/.local/lib/python3.10/site-packages/fastapi/openapi/docs_bak.py
+# cp ./docs.py ~/.local/lib/python3.10/site-packages/fastapi/openapi/
 uvicorn main:app --reload --host 0.0.0.0 --port 8000
